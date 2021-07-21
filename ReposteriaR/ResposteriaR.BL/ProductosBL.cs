86b3ed0d186faeb85bd135bf7970e4f957be339c
Shelly <Shelly@DESKTOP-9HPSIB0>
2021-07-21 12:28:33 -0600

    Agregando Entity Framework Code First

--- ReposteriaR/ResposteriaR.BL/ProductosBL.cs
+++ ReposteriaR/ResposteriaR.BL/ProductosBL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ResposteriaR.BL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,30 +9,21 @@ namespace ReposteriaR.BL
 {
     public class ProductosBL
     {
+        Contexto _contexto;
+        public List<Producto> ListadeProductos { get; set; }
+
+
+        public ProductosBL()
+        {
+            _contexto = new Contexto();
+            ListadeProductos = new List<Producto>();
+        }
+
         public List<Producto> ObtenerProductos()
         {
-            var producto1 = new Producto();
-            producto1.Id = 1;
-            producto1.Descripcion = "Pastel tres leches";
-            producto1.Precio = 350;
-
-            var producto2 = new Producto();
-            producto2.Id = 2;
-            producto2.Descripcion = "Pastel helado de chocolate";
-            producto2.Precio = 300;
-
-            var producto3 = new Producto();
-            producto3.Id = 3;
-            producto3.Descripcion = "Pastel de caramelo";
-            producto3.Precio = 250;
-
-            var listadeProductos = new List<Producto>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
-
-
-            return listadeProductos;
+            ListadeProductos = _contexto.Productos.ToList();
+            return ListadeProductos;
         }
     }
 }
+
